9e293ae3dfaa08272795759ff6735f0de619603c
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-11 20:45:41 -0500

    updated src dir and lib dir

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
+const fileDirectory = path.resolve(__dirname, "dist");
+const filePath = path.join(fileDirectory, "index.html");
 
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const generateHTML = require("./lib/generateHTML");
+const render = require("./lib/generateHTML");
 
-const employees = [];
+let employees = [];
 
 const questions = [
   {
@@ -32,36 +35,100 @@ const questions = [
   },
 ];
 
-function init() {
-  inquirer.prompt(questions).then(function (inquirerResponses) {
-    if (inquirerResponses.role === "Engineer") {
-      inquirer.prompt({
-        type: "input",
-        name: "github",
-        message: "What is the engineer's Github Username? (Required)",
-        validate: (githubInput) => {
-          if (githubInput) {
-            return true;
-          } else {
-            console.log("Please enter a GitHub username!");
-            return false;
-          }
-        },
+managerQuestions = [
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is the manager's office number? (Required)",
+  },
+];
+
+engineerQuestions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is the engineer's Github Username? (Required)",
+    validate: (githubInput) => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log("Please enter a GitHub username!");
+        return false;
+      }
+    },
+  },
+];
+
+internQuestions = [
+  {
+    type: "input",
+    name: "school",
+    message: "What school is the intern from? (Required)",
+  },
+];
+
+const newEmployee = () => {
+  inquirer.prompt(questions).then((response) => {
+    let name = response.name;
+    let id = response.id;
+    let email = response.email;
+    let role = response.role;
+    let officeNumber;
+    let github;
+    let school;
+
+    if (role === "Engineer") {
+      inquirer.prompt(engineerQuestions).then((response) => {
+        github = response.github;
+        let employee = new Engineer(name, id, email, github);
+        employees.push(employee);
+        createAnother();
       });
-    } else if (inquirerResponses.role === "Manager") {
-      inquirer.prompt({
-        type: "input",
-        name: "officeNumber",
-        message: "What is the manager's office number? (Required)",
+    } else if (role === "Manager") {
+      inquirer.prompt(managerQuestions).then((response) => {
+        officeNumber = response.officeNumber;
+        let employee = new Manager(name, id, email, officeNumber);
+        employees.push(employee);
+        createAnother();
       });
-    } else if (inquirerResponses.role === "Intern") {
-      inquirer.prompt({
-        type: "input",
-        name: "school",
-        message: "What school is the intern from? (Required)",
+    } else if (role === "Intern") {
+      inquirer.prompt(internQuestions).then((response) => {
+        school = response.school;
+        let employee = new Intern(name, id, email, school);
+        employees.push(employee);
+        createAnother();
       });
     }
   });
+};
+
+function init() {
+  createAnother();
 }
+
+const createAnother = () => {
+  inquirer
+    .prompt({
+      type: "confirm",
+      name: "createAnother",
+      message: "Would you like to add an employee?",
+    })
+    .then(function (data) {
+      if (data.createAnother) {
+        newEmployee();
+      } else {
+        const renderHTML = (employees) => {
+          fs.writeFile(filePath, render(employees), "utf8", function (err) {
+            if (err) {
+              return console.log(err);
+            }
+
+            console.log("Success!");
+          });
+        };
+        renderHTML();
+      }
+    });
+};
 
 init();
